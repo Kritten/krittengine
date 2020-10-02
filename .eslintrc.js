e8b619931462b41f0293d9fe03f1e11f81ce15ce
kritten <bugrpkvvuqrkcyorhobhnkiznizmvb>
2020-10-02 20:35:18 +0200

    *eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,13 +2,13 @@ module.exports = {
   env: {
     browser: true,
   },
-  extends: ['prettier/@typescript-eslint', 'airbnb-typescript/base'],
+  extends: ['airbnb-typescript/base', 'plugin:prettier/recommended', 'plugin:@typescript-eslint/recommended', 'prettier/@typescript-eslint'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: './tsconfig.json',
   },
   ignorePatterns: ['/*.*', 'deprecated/', 'dist/'],
-  plugins: ['@typescript-eslint'],
+  plugins: ['@typescript-eslint', 'prettier'],
   rules: {
     'max-len': [
       'error',
